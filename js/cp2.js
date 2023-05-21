7b73e7857b1c564d3e481ac7fbd18373841689b6
santos-nikolas <rm551566@fiap.com.br>
2023-05-21 12:56:56 -0300

    realizando exercícios 2 e 3

--- js/cp2.js
+++ js/cp2.js
@@ -17,7 +17,7 @@ function getRandomNumber(min, max) {
     // Altera a cor do texto para preto
     text.style.color = '#000';
     text.style.top = '30%'
-    // Aguarda 1,5 segundos e depois restaura a visibilidade da imagem e a cor do texto
+    // Aguarda 0,5 segundo e depois restaura a visibilidade da imagem e a cor do texto
     setTimeout(function() {
       image.style.display = 'block';
       text.style.color = '#fff';
@@ -25,6 +25,56 @@ function getRandomNumber(min, max) {
     }, 500);
   }
   
-  // Chama a função toggleImageVisibility a cada 0,5 segundos
+  // Chama a função toggleImageVisibility a cada 200 milisegundos
   setInterval(toggleImageVisibility, 200);
+
+// Variável de controle para verificar o estado atual da ordem das divs
+var reverseOrder = false;
+
+// Event listener para o botão
+var reverseButton = document.getElementById('reverseButton');
+reverseButton.addEventListener('click', reverseDivs);
+
+// Função para inverter a ordem das divs
+function reverseDivs() {
+  var container = document.querySelector('.container');
+  var divs = container.children;
+
+  var divArray = Array.from(divs);
+
+  // Inverte a ordem dos elementos na matriz
+  divArray.reverse();
+
+  // Limpa o conteúdo atual do container
+  container.innerHTML = '';
+
+  // Adiciona as divs invertidas de volta ao container
+  divArray.forEach(function (div, index) {
+    container.appendChild(div);
+    if (reverseOrder) {
+      div.getElementsByClassName('imagem-texto')[0].textContent = 'Div-' + (divArray.length - index);
+    } else {
+      div.getElementsByClassName('imagem-texto')[0].textContent = 'Div-' + (index + 1);
+    }
+  });
+
+  // Inverte o valor da variável de controle
+  reverseOrder = !reverseOrder;
+}
+
+
+var darkModeToggle = document.getElementById('darkModeToggle');
+darkModeToggle.addEventListener('click', toggleDarkMode);
+
+function toggleDarkMode() {
+  var container = document.querySelector('.container');
   
+  // Verifica se o modo escuro está ativado
+  if (container.style.backgroundColor === 'rgb(221, 217, 217)') {
+    // Modo escuro ativado, define a cor do modo escuro
+    container.style.backgroundColor = 'black';
+  } else {
+    // Modo escuro desativado, remove a cor do modo escuro
+    container.style.backgroundColor = 'rgb(221, 217, 217)';
+  }
+}
